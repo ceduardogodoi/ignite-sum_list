55ce7a4ca3e0b64c7e393ddba632dc9d18b7f061
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-12-06 21:51:47 -0300

    Introdução ao módulo Enum

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,6 +1,12 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
+  # def call_enum(list), do: Enum.sum(list)
+  # def call_enum(list), do: Enum.max(list)
+  # def call_enum(list), do: Enum.min(list)
+  # def call_enum(list), do: Enum.map(list, fn elem -> elem + 1 end)
+  def call_enum(list), do: Enum.any?(list, fn elem -> elem > 5 end)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
